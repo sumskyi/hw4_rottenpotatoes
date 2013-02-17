42455a6a5326ddb6b12a977f2d13cf00386b3ec8
vlad <sumskyi@gmail.com>
2013-02-17 19:45:06 +0200

    spec/helpers

--- spec/helpers/movies_helper_spec.rb
+++ spec/helpers/movies_helper_spec.rb
@@ -1,12 +1,7 @@
 require 'spec_helper'
 
-class MyClass
-  extend MoviesHelper
-end
-
 describe MoviesHelper do
   it '.oddness' do
-    MyClass.oddness(1).should be_true
+    helper.oddness(1).should be_true
   end
-
 end
